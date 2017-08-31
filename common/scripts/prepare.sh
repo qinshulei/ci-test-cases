c253e109706fe6f7544293541a1552a8da6c6bc7
qinsl0106 <qinsl0106@thundersoft.com>
2017-08-31 16:54:43 +0800

    sleep more time to wait apt ready

--- common/scripts/prepare.sh
+++ common/scripts/prepare.sh
@@ -1,11 +1,29 @@
 #!/bin/bash
 
-sleep 5
+sleep 10
 
 # wait the apt ready.
 while (( $(ps -ef | grep '[a]pt-get' | wc -l) > 0 ));do
     echo -n "."
     sleep 1
 done
+
+sleep 10
+
+# wait the apt ready.
+while (( $(ps -ef | grep '[a]pt-get' | wc -l) > 0 ));do
+    echo -n "."
+    sleep 1
+done
+
+sleep 10
+
+# wait the apt ready.
+while (( $(ps -ef | grep '[a]pt-get' | wc -l) > 0 ));do
+    echo -n "."
+    sleep 1
+done
+
+ps aux | grep '[a]pt'
 
 lava-test-case prepare-wait-apt --result pass
